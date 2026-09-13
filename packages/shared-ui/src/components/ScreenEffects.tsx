39643477a189ec106e24ec8e2f8c898f5195db47
Public Name <azerbaev87@gmail.com>
2026-09-13 18:12:22 +0500

    Add animated VHS scan pass

--- packages/shared-ui/src/components/ScreenEffects.tsx
+++ packages/shared-ui/src/components/ScreenEffects.tsx
@@ -4,7 +4,8 @@ import React, { memo } from 'react';
 export const ScreenEffects = memo(() => (
   <div className="ne-screen-fx" aria-hidden="true">
     <span className="ne-screen-fx__scanlines" />
-    <span className="ne-screen-fx__noise" />
+    <span className="ne-screen-fx__scan-pass" />
+    <span className="ne-screen-fx__static" />
     <span className="ne-screen-fx__vignette" />
   </div>
 ));
